9a4c91422ac57067c57f0eb2ea7fd8b54615004e
Melissande <melissande.lemairevalle@edu.ece.fr>
2026-04-30 16:57:19 +0200

    fcn a modified and text to describe it in code main

--- Code/main.m
+++ Code/main.m
@@ -6,8 +6,9 @@ output = true;
 
 try
     outputArg1 = fcn_a(inputArg1);
+    // add 1 then times 2
 
-    outputArg2 = fnc_b(inputArg2);
+    outputArg2 = fcn_b(inputArg2);
 
 catch
     output = false;
